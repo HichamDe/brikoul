103240202ec17761b592a00f895a0038e0b29a97
HICHAM IAMIRI <89263522+HichamDe@users.noreply.github.com>
2023-06-25 09:48:10 +0100

    login page request page

--- env/files.js
+++ env/files.js
@@ -2,7 +2,9 @@ const path = require("path");
 const index = path.join(__dirname,"../public/pages/index.html");
 const dashboard_driver = path.join(__dirname,"../public/pages/dashboard_driver.html");
 const dashboard_client = path.join(__dirname,"../public/pages/dashboard_client.html");
+const login = path.join(__dirname,"../public/pages/login.html");
 const maps = path.join(__dirname,"../test/dashboard_driver.html");
+const requesttaxi = path.join(__dirname,"../public/pages/requesttaxi.html");
 
 
-module.exports = { dashboard_client, dashboard_driver , index, maps};
+module.exports = { dashboard_client, dashboard_driver , index, maps,login,requesttaxi};
--- server.js
+++ server.js
@@ -3,12 +3,22 @@ const logger = require("morgan");
 const bodyParser = require("body-parser");
 const PORT = 8000;
 const app = express();
-const {dashboard_client,dashboard_driver,index,maps} = require("./env/files");
+const {dashboard_client,dashboard_driver,index,maps,login,requesttaxi} = require("./env/files");
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.use(express.static('public'));
 
 app.get("/",(req,res)=>{
     res.sendFile(index);
+});
+app.get("/request",(req,res)=>{
+    res.sendFile(requesttaxi);
+});
+app.get("/login",(req,res)=>{
+    res.sendFile(login);
 });
 app.get("/dashboard_client",(req,res)=>{
     res.sendFile(dashboard_client);
@@ -20,4 +30,27 @@ app.get("/maps",(req,res)=>{
     res.sendFile(maps)
 })
 
-app.listen(PORT,console.log(`Server Running at ${PORT}`));
+let clients = [];
+let drivers = [];
+
+io.on("connection",(socket)=>{
+
+    // online
+    socket.on("online",(data)=>{
+        let {id,type,longitude,latitude} = data;
+        if(type=="client") clients.push({id,type,latitude,longitude});
+        else if (type=="driver") drivers.push({id,type,latitude,longitude});
+
+        // send to the clients the updates
+        clients.forEach(client => {
+            socket.broadcast.emit("all-working-drivers",drivers)
+        });
+    });
+    
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+})
+
+server.listen(PORT,console.log(`Server Running at ${PORT}`));
